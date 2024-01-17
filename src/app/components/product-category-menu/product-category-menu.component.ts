1128d558d5d0f4be8383c4e2bb4b08862f9bef07
kaiqi <zhang.kaiqi1@northeastern.edu>
2024-01-16 17:05:36 -0800

    added search function

---
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { ProductCategory } from '../../common/product-category';
+import { isNgContainer } from '@angular/compiler';
+
+@Component({
+  selector: 'app-product-category-menu',
+  templateUrl: './product-category-menu.component.html',
+  styleUrl: './product-category-menu.component.css'
+})
+
+export class ProductCategoryMenuComponent {
+
+  productCategories: ProductCategory[] = [];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(){
+    this.listProductCategories();
+  }
+
+  listProductCategories() {
+    
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product Categories= ' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    );
+  }
+
+}
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,23 +1,62 @@
 import { Component } from '@angular/core';
 import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list-table.component.html',
+  templateUrl: './product-list-grid.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
 
   products: Product[] = [];
-  constructor(private productService: ProductService){ }
+  currentCategoryId: number = 1;
+  searchMode: boolean=false;
 
-  ngOnInit(): void {
-    this.listProducts();
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProduct();
+    }
+  }
+  
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProduct() {
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+    // get the "id" param string. Convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!; // ! is the non-null assertion operator
+    }
+    else {
+    // no category id available... default to category id 1
+      this.currentCategoryId = 1;
+    }
+    // now we get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
