ca7921192f7691dfec3f77347029b7a698da87e1
kaiqi <zhang.kaiqi1@northeastern.edu>
2024-01-23 10:50:53 -0800

    added security login/logout with okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,19 @@
+export default {
+
+
+
+    oidc: {
+
+        clientId: '9UgBtNaqme6iqPZAqZyo1AsLtsIA9nu8',
+
+        issuer: 'https://dev-gutzpwdqgeqb1g67.us.auth0.com/oauth2/default',
+
+        redirectUri: 'http://localhost:4200/login/callback',
+
+        scopes: ['openid', 'profile', 'email']
+
+    }
+
+
+
+}
